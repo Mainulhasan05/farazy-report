afc7b6041c974549c5ddb7d91944b853d69cb8e8
Md. Mainul Hasan <mdrifatbd5@gmail.com>
2023-09-06 18:11:59 +0600

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,8 @@ const index = () => {
   const [patient, setPatient] = useState(null)
   const [invoiceNo, setInvoiceNo] = useState('DIA230904101')
   const [testResults, setTestResults] = useState([])
+  const [groupResult, setGroupResult] = useState([])
+
   const loadTestName = async () => {
     try {
       // Fetch the first endpoint
@@ -17,7 +19,7 @@ const index = () => {
       }
       const data1 = await res1.json();
       console.log(data1);
-  
+
       // Fetch the second endpoint
       const res2 = await fetch(`${process.env.API}/api/Investigation/GetInvoiceInfo?InvNo=${invoiceNo}`);
       if (!res2.ok) {
@@ -25,7 +27,6 @@ const index = () => {
       }
       const data2 = await res2.json();
       console.log(data2);
-  
       if (data2.length > 0) {
         setPatient({
           patientName: data2[0].patientName,
@@ -34,8 +35,26 @@ const index = () => {
           tests: data1?.[0]?.itemDescription,
         });
       }
-  
+
       setTestResults(data2);
+      // data2 array each element contains fields invNo, pCode
+      // call loadResults for each of them and store the result in new array
+      // const results = await Promise.all(
+      //   data2.map((item) => loadResults(item.invNo, item.pCode))
+      // );
+      // console.log(results);
+
+
+      const groupResult = await Promise.all(
+        data2.map(async (item) => {
+          const result = await loadResults(item.invNo, item.pCode);
+          return { item, result };
+        })
+      );
+      console.log(groupResult);
+
+      setGroupResult(groupResult);
+
     } catch (error) {
       console.error('Error fetching data:', error);
     }
@@ -62,15 +81,20 @@ const index = () => {
     const res = await axios.get(`${process.env.API}/api/Investigation/GetInvestigationGroupName?InvNo=${invoiceNo}`)
     console.log(res.data)
   }
-
+  // http://59.152.61.146:6200/api/Investigation/GetReportDataByTestCode?invNo=DIA230904106&pCode=1028
+  const loadResults = async (invoiceNo, pCode) => {
+    const axiosRes = await axios.get(`${process.env.API}/api/Investigation/GetReportDataByTestCode?invNo=${invoiceNo}&pCode=${pCode}`)
+    console.log(axiosRes.data)
+    return axiosRes.data
+  }
   useEffect(() => {
     console.log(patient)
   }, [patient])
-  
+
 
   return (
     <>
-    
+
       <br />
       <div className="text-center container alert">
         <div>
@@ -82,40 +106,43 @@ const index = () => {
 
         {/* Patient's Info */}
         {
-          testResults.length  > 0 && patient &&
+          testResults.length > 0 && patient &&
           <div className="row">
-          <div className="col-md-6 col-lg-12">
-            <div className="card">
-              <div className="card-header">
-                Patient's Info
-              </div>
-              <div className="card-body">
-                <div className="row">
-                  <div className="col-md-6 col-lg-12">
-                    {/* create a table which will contain patients name phone */}
-                    <table className="table table-bordered">
-                      <tbody>
-                        <tr>
-                          <td>Name</td>
-                          <td>{patient?.patientName}</td>
-                        </tr>
-                        <tr>
-                          <td>Phone</td>
-                          <td>{patient?.telephoneNo}</td>
-                        </tr>
-                        <tr>
-                          <td>Age</td>
-                          <td>{patient?.age}</td>
-                        </tr>
-                      </tbody>
-                    </table>
+            <div className="col-md-6 col-lg-12">
+              <div className="card">
+                <div className="card-header">
+                  Patient's Info
+                </div>
+                <div className="card-body">
+                  <div className="row">
+                    <div className="col-md-6 col-lg-12">
+                      {/* create a table which will contain patients name phone */}
+                      <table className="table table-bordered">
+                        <tbody>
+                          <tr>
+                            <td>Name</td>
+                            <td>{patient?.patientName}</td>
+                          </tr>
+                          <tr>
+                            <td>Phone</td>
+                            <td>{patient?.telephoneNo}</td>
+                          </tr>
+                          <tr>
+                            <td>Age</td>
+                            <td>{patient?.age}</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
         }
+
+
+        <br />
 
 
         {/* Done tests */}
@@ -123,43 +150,74 @@ const index = () => {
           <div className="col-md-6 col-lg-12">
             <div className="card">
               <div className="card-header">
-                 {testResults.length  > 0 ? <div>
-                    <h3>Done Tests</h3>
-                    
-                    
+                {testResults.length > 0 ? <div>
+                  <h3>Done Tests</h3>
 
-                 </div>  : 'No Test Found'}
-              </div>
-              <div className="card-body">
 
-                <div className="row">
-                  <div className="col-md-6 col-lg-12">
-                    {/* create a table which will contain patients name phone */}
-                    <table className="table table-bordered">
-                      <thead>
-                        <tr>
-                          <th>Test Name</th>
-                          <th>Result</th>
-                          <th>Unit</th>
-                          <th>Reference Range</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        {
-                          testResults.map((test, index) => (
-                            <tr key={index}>
-                              <td>{test.shortDesc}</td>
-                              <td>{test.result}</td>
-                              <td>{test.unit}</td>
-                              <td>{test.referenceRange}</td>
-                            </tr>
-                          ))
-                        }
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
+
+                </div> : 'No Test Found'}
               </div>
+
+              {
+                groupResult.map((item, index) => {
+                  return (
+                    <>
+                      <div key={index} className="card-body">
+                        <div className="row">
+                          <div className="col-md-6 col-lg-12">
+
+                            <table className="table table-bordered">
+                              <thead>
+                                <tr>
+                                  <th>Test Name</th>
+                                  <th>{item.item.shortDesc}</th>
+                                </tr>
+                              </thead>
+                              
+                            </table>
+
+                            <br />
+                            
+                            {
+                              item.result.map((item2, index) => {
+                                return (
+                                  <>
+                                    <b>{item2.reportingGroup}</b>
+                                    <table className="table table-bordered">
+                                      <thead>
+                                        {
+                                          item2.reportDatas.map((item3, index) => {
+                                            return (
+                                              <>
+
+                                                <tr>
+                                                  <td>{item3?.aliasName}</td>
+                                                  <td>{item3?.result} {item3?.unit}</td>
+                                                  <td>{item3?.normalValue}</td>
+                                                </tr>
+
+                                              </>
+                                            )
+                                          })
+
+                                        }
+                                      </thead>
+                                    </table>
+
+                                  </>
+                                )
+                              })
+                            }
+                          </div>
+                        </div>
+                      </div>
+                    </>
+                  )
+                })
+              }
+
+
+
             </div>
           </div>
         </div>
