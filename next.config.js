be73d079ab7cd34df31c9dbd0db76c0491cb98f1
Md. Mainul Hasan <mdrifatbd5@gmail.com>
2023-09-06 16:31:10 +0600

    api changed

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env:{
-    API: 'https://59.152.61.146:6200',
+    API: 'http://59.152.61.146:6200',
     REDEPLOY:'https://api.netlify.com/build_hooks/64f85358b4095765f4a11608'
   }
 }
